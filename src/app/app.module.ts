9aa51354ee7dd3b8abfa8998bbd4998b99707cbc
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2020-09-24 01:02:18 -0300

    Iniciado construção do protótipo da tela de lançamentos onde foi criado o componente lancamento-cadastro, foi feito o form e adicionado o componente de calendário nos campos de data de vencimento e de recebimento.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { InputTextModule } from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
 import {TooltipModule} from 'primeng/tooltip';
-
-
+import {CalendarModule} from 'primeng/calendar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MenubarModule} from 'primeng/menubar';
+import { MenubarModule } from 'primeng/menubar';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 import { FormsModule } from '@angular/forms';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 
 @NgModule({
@@ -22,16 +24,20 @@ import { FormsModule } from '@angular/forms';
     LancamentosPesquisaComponent,
     NavbarComponent,
     PessoasPesquisaComponent,
+    LancamentoCadastroComponent,
 
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     InputTextModule,
     ButtonModule,
     TableModule,
     TooltipModule,
     MenubarModule,
     FormsModule,
+    InputTextareaModule,
+    CalendarModule,
     AppRoutingModule
   ],
   providers: [],
